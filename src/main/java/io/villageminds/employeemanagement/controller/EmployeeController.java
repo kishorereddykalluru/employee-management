3a5e23be93834216e04518244ba29c08b28170b9
kishore <kishorereddykalluru@gmail.com>
2021-06-14 23:33:23 +0530

    latest changes

--- src/main/java/io/villageminds/employeemanagement/controller/EmployeeController.java
+++ src/main/java/io/villageminds/employeemanagement/controller/EmployeeController.java
@@ -4,9 +4,9 @@ import io.villageminds.employeemanagement.domain.Employee;
 import io.villageminds.employeemanagement.persistence.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController implements ErrorController {
@@ -14,8 +14,31 @@ public class EmployeeController implements ErrorController {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @GetMapping("/employee")
+    public List<Employee> getEmployees(){
+        return employeeRepository.findAll();
+    }
+
     @GetMapping("/employee/{id}")
     public Employee getEmployeeById(@PathVariable("id") Long id){
-        return employeeRepository.findById(id);
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("employee not found"));
+    }
+
+    @PostMapping("/employee")
+    public String createEmployee(@RequestBody Employee employee){
+        Employee emp = employeeRepository.save(employee);
+        return  emp.getFirstName() + " saved to DB";
+    }
+
+    @PutMapping("/employee")
+    public String updateEmployee(@RequestBody Employee employee){
+        Employee emp = employeeRepository.save(employee);
+        return emp.getFirstName() + " updated and saved to DB";
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public void deleteById(@PathVariable("id") Long id){
+        employeeRepository.deleteById(id);
     }
 }
